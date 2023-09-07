73f48448f2ffc4d5b858fe1792e5b618d303da7f
Katya <capkat@bk.ru>
2023-09-07 10:18:40 +0300

    change tass

--- crawlers.py
+++ crawlers.py
@@ -1,12 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
 from urllib.parse import urljoin
+import feedparser
+import time
 
 
 def get_news_tass():
     url = "https://tass.ru/ekonomika"
-
-    pages = requests.get(url)
+    user_agent = {'User-agent': 'Mozilla/5.0 (Linux; Android 13; CPH2211 Build/TP1A.220905.001) AppleWebKit/537.36 '
+                                '(KHTML, like Gecko) Version/4.0 Chrome/115.0.5790.166 Mobile Safari/537.36'}
+    pages = requests.get(url, headers=user_agent)
 
     soup = BeautifulSoup(pages.text, "lxml")
 
@@ -15,6 +18,7 @@ def get_news_tass():
         title = news.select('span[class^=ds_ext_title]')[0].contents[0]
 
         item = {
+            '_timestamp': int(time.time()),
             'url': news_url,
             'title': title
         }
@@ -22,12 +26,56 @@ def get_news_tass():
 
 
 def get_news_tass_rss():
-    pass
+    url = 'https://tass.ru/rss/v2.xml'
+
+    pages = requests.get(url)
+
+    try:
+        parser = feedparser.parse(pages.text)
+        if feeds_list := parser.entries:
+            print(f'Received feed response for {url}')
+            yield from parse_feed(feeds_list)
+            return
+    except Exception as exep:
+        print(f"Can't parse response from {url}  exception: {exep}")
+        return
+
+
+def parse_feed(feeds_list):
+    for feed in feeds_list:
+
+        result_item = {
+            '_timestamp': int(time.time()),
+            'platform': 'tass',
+            'type': 'news',
+        }
+
+        if pub_date := feed.get('published'):
+            result_item['publication_date'] = pub_date
+
+        if title := feed.get('title'):
+            result_item['title'] = title.strip()
+
+        if url := feed.get('link'):
+            result_item['url'] = urljoin('https://tass.ru', url.strip())
+
+        if categories := [tag.term.strip() for tag in feed.get('tags', [])]:
+            result_item['category'] = categories
+
+        if categories and 'Экономика и бизнес' in categories:
+            yield result_item
 
 
 def get_news_enclosure():
     pass
 
 
-for elem in get_news_tass():
-    print(elem)
+while True:    # TODO retries
+    for elem in get_news_tass():
+        print(elem)
+
+    for elem in get_news_tass_rss():
+        print(elem)
+
+    time.sleep(5)
+
